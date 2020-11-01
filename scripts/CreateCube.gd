fd214e83e872d10fb7493a57aa7191544f78ba46
wu1274704958 <1274704958@qq.com>
2020-11-01 20:01:53 +0800

    create cube

--- scripts/CreateCube.gd
+++ scripts/CreateCube.gd
@@ -8,39 +8,29 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var arr = []
-	arr.resize(Mesh.ARRAY_MAX)
-
-	# PoolVectorXXArrays for mesh construction.
-	var verts = PoolVector3Array()
-	var uvs = PoolVector2Array()
-	var normals = PoolVector3Array()
-	var indices = PoolIntArray()
 	
-	verts.append(Vector3(-1.0,0.0,0.0))
-	verts.append(Vector3(-1.0,1.0,0.0))
-	verts.append(Vector3(1.0,1.0,0.0))
-	verts.append(Vector3(-1.0,0.0,0.0))
-	verts.append(Vector3(1.0,1.0,0.0))
-	verts.append(Vector3(1.0,0.0,0.0))
-	
-	#indices = [ 0,1,2 ]
-	#######################################
-	## Insert code here to generate mesh ##
-	#######################################
+	var st:SurfaceTool = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	st.add_normal(Vector3(0, 0, 1))
+	st.add_uv(Vector2(0, 0))
+	# Call last for each vertex, adds the above attributes.
+	st.add_vertex(Vector3(-1, -1, 0))
 
-	# Assign arrays to mesh array.
-	arr[Mesh.ARRAY_VERTEX] = verts
-	#arr[Mesh.ARRAY_TEX_UV] = uvs
-	#arr[Mesh.ARRAY_NORMAL] = normals
-	#arr[Mesh.ARRAY_INDEX] = indices
-	var mesh = Mesh.new()
-	# Create mesh surface from mesh array.
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arr)
-	Material.new(
+	st.add_normal(Vector3(0, 0, 1))
+	st.add_uv(Vector2(0, 1))
+	st.add_vertex(Vector3(-1, 1, 0))
+
+	st.add_normal(Vector3(0, 0, 1))
+	st.add_uv(Vector2(1, 1))
+	st.add_vertex(Vector3(1, 1, 0))
+	
+	var mesh:ArrayMesh = st.commit()
+	
+	var mat:Material = preload("res://textures/material.tres")
 	
 	var mi = MeshInstance.new()
 	mi.mesh = mesh
+	mi.material_override = mat
 	add_child(mi)
 	pass # Replace with function body.
 
