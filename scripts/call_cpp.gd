9f4038dac5ab45b44c9bebb3d6ad21f67fd14bc2
wws <1274704958@qq.com>
2020-11-02 20:34:13 +0800

    tree .

--- scripts/call_cpp.gd
+++ scripts/call_cpp.gd
@@ -15,16 +15,54 @@ onready var cpp_node = $".."
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("GDNode ready")
-	#translate(Vector3(0,3,0))
-	#var y = 0;
-	#for i in range(5):
-	# call cpp function
-	#var m:MeshInstance = cpp_node.call("create_cube",6,30,4,27,24,0) 
-	#m.translation.y = y;
-	#y += 1;
-	#add_child(m)
+	#test_call()
+	create_tree()
 	pass # Replace with function body.
 
+
+func test_call():
+	translate(Vector3(0,1,0))
+	var y = 0;
+	#call cpp function
+	var m:MeshInstance = cpp_node.call("create_cube",6,30,4,27,24,0) 
+	m.translation.y = y;
+	y += 1;
+	add_child(m)
+	pass
+
+func create_tree():
+	var y = 0;
+	
+	for i in range(6):
+		var m:MeshInstance = cpp_node.call("create_cube",12,30,12,30,12,12) 
+		m.translation.y = y;
+		y += 1;
+		add_child(m)
+	pass
+	
+	var m:MeshInstance = cpp_node.call("create_cube",26,26,26,26,26,26)
+	m.translation.y = y;
+	add_child(m)
+	
+	var m2:MeshInstance = cpp_node.call("create_cube",26,26,26,26,26,26)
+	m2.translation.y = y - 1;
+	m2.translation.x = 1;
+	add_child(m2)
+	
+	var m3:MeshInstance = cpp_node.call("create_cube",26,26,26,26,26,26)
+	m3.translation.y = y - 1;
+	m3.translation.x = -1;
+	add_child(m3)
+	
+	var m4:MeshInstance = cpp_node.call("create_cube",26,26,26,26,26,26)
+	m4.translation.y = y - 1;
+	m4.translation.z = -1;
+	add_child(m4)
+	
+	var m5:MeshInstance = cpp_node.call("create_cube",26,26,26,26,26,26)
+	m5.translation.y = y - 1;
+	m5.translation.z = 1;
+	add_child(m5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
