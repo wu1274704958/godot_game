4849bb9f6c89d99a85602e92c3c36142311bc1b4
wws <1274704958@qq.com>
2020-11-03 18:00:36 +0800

    node3d

--- scripts/center_window.gd
+++ scripts/center_window.gd
@@ -17,12 +17,16 @@ func _ready():
 	self.set_position(Vector2((OS.window_size.x - get_size().x * scale) / 2,(OS.window_size.y - get_size().y * scale)/2));
 	
 	init_texture_3d()
+	#get_parent().remove_child(viewport)
 	pass # Replace with function body.
 
 func init_texture_3d():
 	sprite.position.x = get_size().x / 2;
 	sprite.position.y = get_size().y / 2;
+	viewport.size.x = OS.window_size.x;
+	viewport.size.y = OS.window_size.y;
 	sprite.texture = viewport.get_texture()
+	
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
